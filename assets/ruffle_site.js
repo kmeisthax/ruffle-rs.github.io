887a28543f9750589779342bdadac4ef56bd7a79
David Wendt <dcrkid@yahoo.com>
2019-11-23 14:33:37 -0500

    Make the reveal functional on mobile.

--- assets/ruffle_site.js
+++ assets/ruffle_site.js
@@ -27,5 +27,61 @@
 
     window.customElements.define("site-header", SiteHeader);
 
+    module.SiteHeader = SiteHeader;
+
     window.SiteHeader = module;
+}());
+
+(function () {
+    var module = {};
+    
+    class TableOfContents extends HTMLElement {
+        constructor() {
+            super();
+
+            this.revealed = false;
+        }
+
+        /// Find the reveal button.
+        get reveal_button() {
+            return this.children.item(0);
+        }
+
+        /// Find the table of contents content.
+        get content() {
+            return this.children.item(1);
+        }
+
+        get revealed() {
+            return this.getAttribute("revealed") !== null;
+        }
+
+        set revealed(value) {
+            if (value) {
+                this.setAttribute("revealed", "");
+            } else {
+                this.removeAttribute("revealed");
+            }
+        }
+
+        connectedCallback() {
+            this.revealed = false;
+            this.reveal_button.addEventListener("click", this.reveal_toggle_intent.bind(this));
+            this.content.addEventListener("focusout", this.hide_intent.bind(this));
+        }
+
+        reveal_toggle_intent() {
+            this.revealed = !this.revealed;
+        }
+
+        hide_intent() {
+            this.revealed = false;
+        }
+    }
+
+    window.customElements.define("table-of-contents", TableOfContents);
+
+    module.TableOfContents = TableOfContents;
+
+    window.TableOfContents = module;
 }());
